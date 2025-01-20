21347417210ee4dee8cbca662d1274b287df3774
Santiago Calderon <santiago.contactoyt@gmail.com>
2025-01-20 18:41:32 +0100

    a

--- Unidad3/Ud3Act2/ejercicio2/js/lightbox.js
+++ Unidad3/Ud3Act2/ejercicio2/js/lightbox.js
@@ -1,23 +1,25 @@
-// Elementos clave
+// Obtengo los elemetos que usaré
 const lightboxContainer = document.getElementById('sec-rep');
 const lightboxContent = document.getElementById('lightbox');
 const openLightboxButton = document.getElementById('open-lightbox'); // Suponiendo que tienes un botón para abrir
 
-// Función para abrir el lightbox
+// aqui solo quitamos la clase que oculta el lightbox
 function openLightbox() {
     lightboxContainer.classList.remove('lightbox-hidden');
 }
 
-// Función para cerrar el lightbox
+// aquí hacemos lo contrario a la función de arriba y es añadimos la clase que oculta el lightbox
 function closeLightbox() {
     lightboxContainer.classList.add('lightbox-hidden');
 }
 
-
+// Creamos el evento click sobre button  
 openLightboxButton.addEventListener('click', openLightbox);
 
-// Evento para cerrar al hacer clic fuera del contenido
+// Este evento es para cerrar el lightbox al hacer clic fuera del contenido
 lightboxContainer.addEventListener('click', (e) => {
+    //donde si es diferente alo contenido del lightbox pues se cierra y se pausa el video,
+    // lo de pausar el video es porque los he elegido yo, y se queda guardado en los segundos donde se quedó
     if (!lightboxContent.contains(e.target)) {
         closeLightbox();
         if (!medio.paused && !medio.ended) {
--- Unidad3/Ud3Act2/ejercicio2/js/reproductor.js
+++ Unidad3/Ud3Act2/ejercicio2/js/reproductor.js
@@ -40,7 +40,10 @@ function iniciar() {
 	barra = document.getElementById('barra');
 	progreso = document.getElementById('progreso');
 	play = document.getElementById('play');
+
 	/* obtener los objetos del resto de elementos necesarios */
+
+	// Aquí obtuve los elementos faltantes
 	reiniciar = document.getElementById('reiniciar');
 	retrasar = document.getElementById('retrasar');
 	adelantar = document.getElementById('adelantar');
@@ -50,6 +53,8 @@ function iniciar() {
 
 	play.addEventListener('click', accionPlay, false);
 	/* crear los manejadores de eventos para el resto de botones */
+
+	// Aquí creo que los eventos para los elementos que ateriormente obtuve.
 	reiniciar.addEventListener('click', reiniciarMedio, false);
 	retrasar.addEventListener('click', () => desplazarReproduccion(-5), false);
 	adelantar.addEventListener('click', () => desplazarReproduccion(5), false);
@@ -78,19 +83,25 @@ function reiniciarMedio() {
 	bucle = setInterval(redimensionaBarra, 1000);
 }
 
+// Aqui si por parametos me viene negativo pues se resta a los segundos, si no se suman, y redimesiono la barra
 function desplazarReproduccion(segundos) {
 	medio.currentTime = (medio.currentTime + segundos);
 	redimensionaBarra();
 }
 
+// estaz me guta mucho, pues cada vez que se llama a esta función es para cambiar al contrario de muted
+// y es fácil el cambiar, y también cambio el estado del button
 function silenciarMedio() {
 	medio.muted = !medio.muted; // esto es super simple si es true lo pone a false, y viceversa
 	medio.muted ? silenciar.value = 'escuchar' : silenciar.value = 'silenciar';
 }
 
+
 function volumeMedio(newVolume) {
 	let nuevoVolumen = medio.volume + newVolume;
 	medio.volume = Math.min(Math.max(nuevoVolumen, 0), 1);
+	// aquí nos aseguramos que siempre este en el rango de 0 y 1, para algo similar que con el tiempo,
+	// pues si viene un numero negartivo se resta y no pues se suma
 }
 
 
